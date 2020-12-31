67963af05733e651c2391543c9fd13c3a4f83682
phelixdusengimana <phelixdusengimana@gmail.com>
2020-12-31 21:51:58 +0200

    button

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -10,9 +10,8 @@ function Home(){
                 <img src={CoName} alt="Company name"/>
                 <p>Color is the aspect of things that is caused by differing qualities of light being reflected or emitted by them. To see color, you have to have light. 
                    When light shines on an object some colors bounce off the object and others are absorbed by it.</p>
-           </div>
-
-           
+               <button className="Button mt-4 py-2 px-5 border-0">Get Started </button>
+           </div>   
         </div>
         
         
