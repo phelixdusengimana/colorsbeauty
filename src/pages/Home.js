45bf1452650e4588ed4005c7b33704ac5bc7dfce
phelixdusengimana <phelixdusengimana@gmail.com>
2020-12-31 21:40:50 +0200

    Slogan

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,13 +1,21 @@
 import Navbar from "../components/Navbar";
-
+import CoName from "../img/ColorsBeauty.png";
 function Home(){
   return(
      <div className="Home-container">
         <Navbar/>
-        
-        <div className="home-bg">
 
+        <div className="home-bg">
+           <div className="Slogan">
+                <img src={CoName} alt="Company name"/>
+                <p>Color is the aspect of things that is caused by differing qualities of light being reflected or emitted by them. To see color, you have to have light. 
+                   When light shines on an object some colors bounce off the object and others are absorbed by it.</p>
+           </div>
+
+           
         </div>
+        
+        
      </div>
   );
 }
