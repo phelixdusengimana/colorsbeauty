c9ad8c92ad817f0a298758d511269c24720f093c
phelixdusengimana <phelixdusengimana@gmail.com>
2021-01-01 13:14:21 +0200

    plates

--- src/components/ColorPlate.js
+++ src/components/ColorPlate.js
@@ -3,14 +3,13 @@ import {FiCopy} from "react-icons/fi";
 
 function ColorPlate() {
     return (
-            <div className="card" style={{width: "18rem", height: "12em", margin: "10px 0", padding: "10px", position: "relative"}}>
+            <div className="card" style={{color: "#f5f6fa", width: "18rem", height: "12em", margin: "10px 0", padding: "10px", position: "relative", backgroundColor: "#0061bd", cursor:"pointer"}}>
                 <div className="card-body">
-                    <h5 className="card-title">Card title</h5>
-                    <h6 className="card-subtitle mb-2 text-muted">Card subtitle</h6>
-                   
+                   <small className="card-subtitle mb-2">Color name:  </small>
+                    <h5 className="card-title">#346beb</h5>
                     <div className="bottomed" style={{position:"absolute",bottom:"10px"}}>
-                    <a href="javascript:void(0)" className="card-link"><FiCopy/></a>
-                    <a href="javascript:void(0)" className="card-link"><BiWindowOpen/></a>
+                    <button className="card-link"><FiCopy/></button>
+                    <button className="card-link"><BiWindowOpen/></button>
                     </div>
                 </div>
             </div>
